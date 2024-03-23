f283feb889eee92f6ff7245f7563ce1de81aa423
E-srinibash <ellasrinibash0@gmail.com>
2024-03-23 22:10:34 +0530

    completed all features

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+       remotePatterns:[
+        {
+           protocol:"https",
+           hostname:"rs.cloudinary.com",
+           port: "" 
+        }
+       ] 
+    }
+};
 
 export default nextConfig;
